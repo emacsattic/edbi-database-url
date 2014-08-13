f6d656a97b52d75895cf083ba84f450b7ddb11c8
Malyshev Artem <proofit404@gmail.com>
2014-08-13 23:26:00 +0400

    Add comments section.

--- edbi-database-url.el
+++ edbi-database-url.el
@@ -22,6 +22,17 @@
 
 ;;; Commentary:
 
+;; Specify database url with environment variable
+;;
+;;     M-x setenv RET DATABASE_URL RET pgsql://me:secret@localhost:5678/test
+;;
+;; Connect to you database
+;;
+;;     M-x edbi-database-url
+;;
+;; Optionally you can specify database url by marking region or type
+;; it interactively.
+
 ;;; Code:
 
 (require 'edbi)
